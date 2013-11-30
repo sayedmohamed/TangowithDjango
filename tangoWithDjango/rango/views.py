0ec21a2af33ee72451becde21092203821a5d77d
sayedmohamed <sayed_khaled475@hotmail.com>
2013-11-30 22:25:25 +0200

    crteate template dir

--- tangoWithDjango/rango/views.py
+++ tangoWithDjango/rango/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('hello rango')
+
+
+def about(request):
+    return HttpResponse('rango says:good bye')
