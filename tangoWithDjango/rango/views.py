49d954e8e7211f9078e5ab12be49e8c28d43aaed
sayedmohamed <sayed_khaled475@hotmail.com>
2013-12-01 04:53:02 +0200

    create index-template page

--- tangoWithDjango/rango/views.py
+++ tangoWithDjango/rango/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
 
 
 def index(request):
-    return HttpResponse('hello rango')
+	context=RequestContext(request)
+	return render(request,'rango/index.html',context)
 
 
 def about(request):
--- tangoWithDjango/tangoWithDjango/urls.py
+++ tangoWithDjango/tangoWithDjango/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,3 +13,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/',include('rango.urls')),
 )
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
