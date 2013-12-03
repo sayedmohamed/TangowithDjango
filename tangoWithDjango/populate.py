32cbb23c028076e0c14be88ec1581e5a55563f43
sayedmohamed <sayed_khaled475@hotmail.com>
2013-12-03 04:43:30 +0200

    create catefory page

--- tangoWithDjango/populate.py
+++ tangoWithDjango/populate.py
@@ -5,40 +5,40 @@ def populate():
 
 	add_page(cat=python_cat,
 		title="official python tutorial" , 
-		url="http://docs.python.org/2/tutorial/")
+		url="http://docs.python.org/2/tutorial/",views=244)
 
 	add_page(cat=python_cat,
 		title="how to think like a computer scientist",
-		url="http://www.greenteapress.com/thinkpython")
+		url="http://www.greenteapress.com/thinkpython",views=155)
 
 	add_page(cat=python_cat,
 		title="learn python in 10 minutes",
-		url="http://www.korokithakis.net/tutorials/python/")
+		url="http://www.korokithakis.net/tutorials/python/",views=373)
 
 
 	django_cat=add_cat(name='django',views=64,likes=32)
 
 	add_page(cat=django_cat,
 		title="officical django tutorial",
-		url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+		url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",views=232)
 
 	add_page(cat=django_cat,
 		title="django rocks",
-		url="http://www.djangorocks.com")
+		url="http://www.djangorocks.com",views=211)
 
 	add_page(cat=django_cat,
 		title="how to tango with django",
-		url="http://www.tangowithdjango.com/")
+		url="http://www.tangowithdjango.com/",views=122)
 
 	frame_cat=add_cat(name="other framework",views=32,likes=16)
 
 	add_page(cat=frame_cat,
 		title="bottle",
-		url="http://bottlepy.org/docs/dev/")
+		url="http://bottlepy.org/docs/dev/",views=177)
 
 	add_page(cat=frame_cat,
 		title="flask",
-		url="http://flask.pocoo.org")
+		url="http://flask.pocoo.org",views=188)
 
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
@@ -49,7 +49,7 @@ def add_page(cat,title,url,views=0):
 	return p
 
 def add_cat(name,views,likes):
-	c=Category.objects.get_or_create(name=name,views=views,title=title)[0]
+	c=Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
 	return c
 
 
--- tangoWithDjango/rango/views.py
+++ tangoWithDjango/rango/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
+from rango.models import Category,Page
 
 
 def index(request):
 	context=RequestContext(request)
-	return render(request,'rango/index.html',context)
+	category_list=Category.objects.order_by('-likes')[:5]
+	context_dict={'categories':category_list}
+
+	for category in category_list:
+		category.url=category.name.replace(' ','_')
+
+	pages=Page.objects.order_by('-views')[:5]
+	context_dict['pages']=pages
+	
+	return render(request,'rango/index.html',context_dict)
 
 
 def about(request):
     return HttpResponse('rango says:good bye')
+
+
+def category(request,category_name_url):
+	category_name=category_name_url.replace('_',' ')
+	context_dict={'category_name':category_name}
+
+	try:
+		category=Category.objects.get(name=category_name)
+		pages=Page.objects.filter(category=category)
+		context_dict['category']=category
+		context_dict['pages']=pages
+	except Category.DoesNotExist:
+		pass
+
+	return render(request,'rango/category.html',context_dict)
